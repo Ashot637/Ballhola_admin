781f3b02f288d369d92884631d882ad381119f98
Ashot637 <ashotmartirosyan637@gmail.com>
2024-03-22 21:43:26 +0400

    Images url change to server

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://146.190.127.106/';
+export const BASE_URL = 'http://146.190.127.106/service/';
 
 const instance = axios.create({
-  baseURL: `${BASE_URL}service/api/v2`,
+  baseURL: `${BASE_URL}api/v2`,
 });
 
 instance.interceptors.request.use((config) => {
