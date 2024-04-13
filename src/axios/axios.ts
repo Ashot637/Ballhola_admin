2416a926004046c6b4ce9bdcebc7812d1caaca95
Ashot637 <ashotmartirosyan637@gmail.com>
2024-04-13 12:38:54 +0400

    https

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,13 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const BASE_URL = 'http://146.190.127.106/service/';
+export const BASE_URL = "https://ballhola.app/service/";
 
 const instance = axios.create({
   baseURL: `${BASE_URL}api/v2`,
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    "accessToken"
+  )}`;
   return config;
 });
 
