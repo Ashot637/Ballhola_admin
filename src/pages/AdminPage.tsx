2353b67c273653856f076729159e8ccb57bf2e79
rimaafrikyan <afrikyanrima@gmail.com>
2024-08-14 12:33:17 +0400

    fixed refresh issue

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -8,11 +8,9 @@ const AdminPage = () => {
   const { user, status } = useSelector(selectAuth);
   const navigate = useNavigate();
 
-  // console.log(user)
-
-  // useEffect(() => {
-  //   localStorage.getItem('accessToken') ? navigate("/admin/dashboard/games") : navigate("/admin/login");
-  // });
+  useEffect(() => {
+    user ? navigate("/admin/dashboard/games") : navigate("/admin/login");
+  });
 
   return (
     <>
